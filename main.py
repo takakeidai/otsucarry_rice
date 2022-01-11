0c0e68a3864d4db9dcad1cd56b2be8085e85764b
takahiro-s <pekeidai.ts@gmail.com>
2022-01-11 14:07:13 +0900

    get_all_extended_wordの実行文の作成

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 
-from app.models.extend_word import get_extend_word
-from app.models.extend_word import create_extend_word
+from app.models.extend_word import get_all_extended_word
 
 if __name__=='__main__':
-    
-    word = 'おはよう'
-    print(get_extend_word(word, create_extend_word))
+    print(get_all_extended_word)
 
 # end of line break
